1e457f4e238c69e71a776b68a63061e922e3d61a
Morka <Nief@mx.at>
2012-10-15 21:43:58 +0200

    Test file Cleaned Up

--- Test.java
+++ Test.java
@@ -4,35 +4,18 @@ import java.text.SimpleDateFormat;
 public class Test {
 
 	public static void main(String[] args){
-<<<<<<< HEAD
-<<<<<<< HEAD
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.mm.dd");
-=======
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy");
->>>>>>> added Test cases
-=======
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy.mm.dd");
->>>>>>> test
-
 		
 		Calendar time1 = Calendar.getInstance();
 		Calendar time2 = Calendar.getInstance();
 		Calendar time3 = Calendar.getInstance();
 
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd.mm.yyyy");
+
 		
 		time1.set(2011, 05, 30);
 		time2.set(2010, 06, 23);
 		time3.set(2012, 06, 23);
-<<<<<<< HEAD
-<<<<<<< HEAD
-		
-=======
 
->>>>>>> Specific Rehearsal not working
-=======
-		
->>>>>>> test
-		
 		Band band = new Band();
 		
 		Gig gig = new Gig(15, "Wien", time1, 20);
@@ -58,14 +41,11 @@ public class Test {
 		System.out.println(gig);
 		System.out.println(dateFormat.format(song.getEndDate().getTime()));
 		System.out.println(song);
-<<<<<<< HEAD
 		
 		System.out.println('\n' + "Specific Rehearsals");
 		System.out.println("At time 30,05,2011: " + band.showRehearsals(time2, time3));
 		System.out.println("At time 30,05,2011: " + band.showRehearsals(time1));
-=======
->>>>>>> added Test cases
-		
+
 		System.out.println('\n' + "Specific Rehearsals");
 		System.out.println("At time 30,05,2011: " + band.showRehearsals(time2, time3));
 		System.out.println("At time 30,05,2011: " + band.showRehearsals(time1));
